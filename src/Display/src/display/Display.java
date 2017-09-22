31e03cafbc1c0a2d30d5a1def20186fdf9dda2e9
LAPTOP-R2DJIDV0\austi <austin.forsling@hotmail.com>
2017-09-21 22:51:55 -0600

    Java file added

---
+++ src/Display/src/display/Display.java
@@ -0,0 +1,11 @@
+package display;
+
+public class Display {
+
+    public static void main(String[] args) {
+        System.out.println("Austin Forsling\n");
+        System.out.println("C++\n");
+        System.out.println("I started learning about software because I wanted to create video games, but after a while that stopped being the reason. Now, I just enjoy creating things and I'm not sure exactly where I'm going with it right now.");
+    }
+    
+}
